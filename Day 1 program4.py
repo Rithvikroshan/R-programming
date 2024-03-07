da1482636b590a478411a93b5f13d95bef226277
Rithvikroshan <113426098+Rithvikroshan@users.noreply.github.com>
2024-03-07 21:13:29 +0530

    output

---
+++ Day 1 program4.py
@@ -0,0 +1,7 @@
+# Read the CSV file
+file_path <- "your_file.csv"  # Replace with the actual path to your CSV file
+data <- read.csv(file_path)
+
+# Display the content of the CSV file
+print("CSV File Content:")
+print(data)
