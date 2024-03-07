c3ff5a1a2984f187d3befa763fe5c1e5e0fd2aea
Rithvikroshan <113426098+Rithvikroshan@users.noreply.github.com>
2024-03-07 21:12:49 +0530

    output

---
+++ Day 1 program2.py
@@ -0,0 +1,10 @@
+# Create three vectors
+a <- c(1, 2, 3)
+b <- c(4, 5, 6)
+c <- c(7, 8, 9)
+
+# Combine vectors into a matrix
+combined_matrix <- cbind(a, b, c)
+
+# Print the matrix
+print(combined_matrix)
