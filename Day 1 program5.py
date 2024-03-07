50b074984cb6878ac581a925651cba9a901dc2b8
Rithvikroshan <113426098+Rithvikroshan@users.noreply.github.com>
2024-03-07 21:14:16 +0530

    output

---
+++ Day 1 program5.py
@@ -0,0 +1,9 @@
+# Create three vectors
+numeric_vector <- c(1.5, 2.3, 3.7)
+character_vector <- c("apple", "banana", "orange")
+logical_vector <- c(TRUE, FALSE, TRUE)
+
+# Display content and types of vectors
+cat("Numeric Vector:", numeric_vector, "\tType:", typeof(numeric_vector), "\n")
+cat("Character Vector:", character_vector, "\tType:", typeof(character_vector), "\n")
+cat("Logical Vector:", logical_vector, "\tType:", typeof(logical_vector), "\n")
